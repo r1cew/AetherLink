5a2b5e74d2993c1dcfd14224a8777fecfd58a2c4
Ishida <faller132@bk.ru>
2026-05-24 00:07:27 +0500

    update git ignore #2

--- aetherlink-phone/src/router/index.ts
+++ aetherlink-phone/src/router/index.ts
@@ -6,12 +6,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
+      path: "/main",
       name: "auth",
       component: Authorization,
     },
     {
-      path: "/main",
+      path: "/",
       name: "main",
       component: MainPage,
     },
